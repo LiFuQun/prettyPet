013fbf330c940c92d9b233d71271acca11fec2fc
liFuqun <2657450020@qq.com>
2020-06-20 20:11:45 +0800

    zhang 反馈写入数据库

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -16,4 +16,17 @@ class Index extends Controller
         $this->assign('data',$data);
         return $this->fetch('contact');
     }
+
+    public function dealmsg(Request $request){
+        $data['name'] = $request->param('name');
+        $data['email'] = $request->param('email');
+        $data['them'] = $request->param('subject');
+        $data['msg'] = $request->param('message');
+        $res = Db::table('msg')->insert($data);
+        if($res){
+            echo '<script>alert("提交成功");history.go(-1)</script>';
+        }else{
+            echo '<script>alert("提交失败,需重新提交");history.go(-1)</script>';
+        }
+    }
 }
