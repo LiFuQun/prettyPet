7d7373aaba0c7f89f7abd908b4d48fc00ba060c5
liFuqun <2657450020@qq.com>
2020-06-20 17:39:05 +0800

    zhang 添加反馈表

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,6 +8,12 @@ class Index extends Controller
 {
     public function index()
     {
+        return $this->contact();
+    }
 
+    public function contact(){
+        $data =  Db::table('website')->find();
+        $this->assign('data',$data);
+        return $this->fetch('contact');
     }
 }
